f7b7a442a9df679f20fb763e69c62472fd02c66f
Dharmishtha patwariya <dharmishthad260@gmail.com>
2023-06-28 21:52:56 +0530

    first commit

--- script.js
+++ script.js
@@ -15,5 +15,21 @@ function addTask(){
 
     }
     inputBox.value = "";
+    saveData();
 }
-listContainer.addEventListener("click", function(e))
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
